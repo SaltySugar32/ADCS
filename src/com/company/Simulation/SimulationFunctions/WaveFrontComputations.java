a4fa56e886f856ec4a57ffc5bb9edc79a7cb7e1e
P0wderGang3r <skopetskiy.ts@gmail.com>
2021-12-13 18:43:52 +1000

    Наводящие на будущий функционал комментарии, правки в формулах.

--- src/com/company/Simulation/SimulationFunctions/WaveFrontComputations.java
+++ src/com/company/Simulation/SimulationFunctions/WaveFrontComputations.java
@@ -28,7 +28,31 @@ public class WaveFrontComputations {
     /**
      * Формула для вычисления скорости новообразованной ударной волны
      */
-    public static double computeNewWaveFrontSpeed(double speedLeft, double speedRight, double deltaMovLeft, double deltaMovRight, double deltaMovCurr) {
-        return speedLeft * speedLeft - (speedLeft * speedLeft - speedRight * speedRight) * (deltaMovLeft) / (deltaMovCurr - deltaMovRight);
+    public static double computeNewWaveFrontSpeed(double speedLeft, double speedRight, double deltaDisplacementPos, double deltaDisplacementNeg) {
+        //return sqrt( a^2 - (a^2 - b^2) * (U^+_,x) / (U^+_,x - U^-_,x) )
+        return speedLeft * speedLeft - (Math.pow(speedLeft, 2) - Math.pow(speedRight, 2)) * (deltaDisplacementPos) / (deltaDisplacementPos - deltaDisplacementNeg);
     }
+
+    public static double computeWaveDisplacement() {
+        return 0;
+    }
+
+    //Вычисление системы уравнений из четырех элементов
+    //Предыдущий = новый;
+    //Новый = следующий.
+    //Преобразуем два уравнения в системе так, чтобы они приняли следующий вид:
+    //Предыдущий = новый:
+    //  Равенство членов 0-й степени производной
+    //  Равенство членов 1-й степени производной
+    //Новый = следующий:
+    //  Равенство членов 0-й степени производной
+    //  Равенство членов 1-й степени производной
+
+    //В данных системах имеем четыре неизвестные: A1_i, A2_i, A0_i, V_i
+    //Выражаем из формулы поиска скорости волнового фронта U^-_,x === A1_i
+    //Подставляем на соответствующее место выраженное уравнение (ручками нужно предварительно всё подготовить)
+    //Таким образом получаем все четыре неизвестные
+
+    //Вопросы остались, но хоть понять уже легче, что тут творится, блэт
+
 }
